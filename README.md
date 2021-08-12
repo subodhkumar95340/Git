b36c22916c099f45364d6b0bfae680554e3f0de9
subodh <47905327+subodhkumar95340@users.noreply.github.com>
2021-08-12 08:43:48 +0530

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-#Demo
+# node-on-my-own
+Simple node http server
+
+
+Checking out Node.js on my own using this Medium guide
+
+
+
+https://medium.com/@saurabhkumar_4718/a-simple-nodejs-server-without-express-js-6773122d365f
+
+Also, official documentation can be found here
+
+https://nodejs.org/api/http.html
+
+I can then refer back to this as I move onto projects that go more in-depth.
+
+To start the server
+```
+node app.js
+```
+To quit the server
+
+### Ctrl + C
---
+++ app.js
@@ -0,0 +1,46 @@
+//let http = require('http');
+
+//1. First try
+//create a server object
+/*http.createServer(function (request, response) {
+  response.write('Hello World!');
+  response.end();
+}).listen(3000, function(){
+  console.log("server start at port 3000"); //the server object listens on port 3000
+})*/
+
+//http is the built in module of Node JS through which data is transferred over HTTP protocols
+//see more built-in modules here: https://nodejs.org/dist/latest-v8.x/docs/api/
+
+
+//2. HTML Response instead of plain text. Only difference http header
+/*http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'}); //http header
+  response.write('<h1>Hello World! This is an HTML response this time.</h1>');
+  response.end()
+}).listen(3000, function(){
+  console.log("server start at port 3000 just because")
+})*/
+
+
+//3. Adding routing
+let http = require('http');
+
+//create a server object
+http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+
+  let url = request.url;
+  if (url === '/about') {
+    response.write('<h4>about page stuff</h4>');
+    response.end();
+  } else if (url === '/contact') {
+    response.write('<h3>contact me through email only</h3>');
+    response.end();
+  } else {
+    response.write('<h1>Hello World</h1><p>this takes any route</p>');
+    response.end();
+  }
+}).listen(3000, function(){
+  console.log("server start at port 3000. conventional")
+})
